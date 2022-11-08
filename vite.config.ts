bd4df944dcbfc6dc242037a78f6cbcd8514858d5
tex <t3xhnolainx@gmail.com>
2022-11-08 14:19:51 +0100

    TEST | Expand vitest configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,14 @@ export default defineConfig({
   test: {
     // Import globals automatically
     globals: true,
+    // Test environment for web apps (DOM access)
+    environment: "happy-dom",
+    // Enable watch mode
+    watch: true,
+    // Custom reporter
+    reporters: "verbose",
+    // Test file names to look for
+    include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
   },
   plugins: [vue()],
 })
