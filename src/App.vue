8fa06e2abf687e52f0821ae71ef80c6875679bba
tex <t3xhnolainx@gmail.com>
2022-11-08 14:08:16 +0100

    TEST | Bootstrap the initial app

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+const items = [
+  {
+    id: 1,
+    age: 12,
+    name: "Item 1",
+  },
+  {
+    id: 2,
+    age: 12,
+    name: "Item 2",
+  },
+  {
+    id: 3,
+    age: 12,
+    name: "Item 3",
+  },
+  {
+    id: 4,
+    age: 12,
+    name: "Item 4",
+  },
+  {
+    id: 5,
+    age: 12,
+    name: "Item 5",
+  },
+  {
+    id: 6,
+    age: 12,
+    name: "Item 6",
+  },
+  {
+    id: 7,
+    age: 12,
+    name: "Item 7",
+  },
+];
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  LOL
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
